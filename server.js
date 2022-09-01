d0929894772473880c8c5d493ba7452ea462745b
Luis Rivera Gonzalez <luisdrg@email.unc.edu>
2022-08-31 23:37:06 -0400

    every task completed.

--- server.js
+++ server.js
@@ -2,27 +2,39 @@
 const http = require('http');
 
 // Require fs module
-const fs = require('fs')
+const fs = require('fs');
 
 // Require minimist module (make sure you install this one via npm).
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-var argv = require('minimist')(process.argv.slice(2));
+var args = require('minimist')(process.argv.slice(2));
 
 // Define a const `port` using the argument from the command line. 
 
 // Make this const default to port 3000 if there is no argument given for `--port`.
+const port = args.port || 3000;
 
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
+const fs = require('fs');
+
+fs.readFile('./public/index.html', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
+
 
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
-
-
-
+process.on('uncaughtException', err => {
+  console.error('There was an uncaught error', err);
+  process.exit(1); // mandatory (as per the Node.js docs)
+});
 
 
 // Define a const `server` as an arrow function using http.createServer. 
@@ -31,14 +43,18 @@ var argv = require('minimist')(process.argv.slice(2));
 // 1. status code 200, 
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
-
-
-
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.end(data)
+})
 
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
-
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`)
+})
 
 
 
